673d27fcfb12cf776e5ab38d970cecca53e3a647
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-25 16:06:05 +0200

    Removed automated testing (no JS)

--- test/mocha-color-setter.js
+++
@@ -1,3 +0,0 @@
-// small helper function to modify base mocha pass text color so it's actually visible with zsh solarized theme
-const colors = require('mocha/lib/reporters/base').colors
-colors.pass = 32
